6104c9a29111cec052dda7259026fa214150b849
Steven Chien <stevenwillchien@gmail.com>
2022-12-06 10:03:09 -0500

    Day 6 solution

---
+++ day6_solution.py
@@ -0,0 +1,12 @@
+line = open('inputs/day6.txt').readline()
+
+def first_n_unique_characters(line: str, n: int) -> int:
+  ptr = n
+
+  while len(set(line[ptr-n:ptr])) != n:
+    ptr += 1
+
+  return ptr
+
+print(f'Solution Part 1: {first_n_unique_characters(line, 4)}')
+print(f'Solution Part 2: {first_n_unique_characters(line, 14)}')
